45bba2b4689f928202473c189120a29a2276e552
konfyt <1665888885@qq.com>
2016-07-26 10:44:20 +0800

    9

---
+++ app/src/main/java/com/konfyt/perfection/activity/GoodInfo.java
@@ -0,0 +1,15 @@
+package com.konfyt.perfection.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.konfyt.perfection.R;
+
+public class GoodInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_info);
+
+    }
+}
